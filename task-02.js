66ad0ef5435ad74b5ce2322a4f2220b07c78fcc7
Hezekiah Oyemade <oyedey@gmail.com>
2021-03-28 16:02:24 +0100

    task 0.2

---
+++ task-02.js
@@ -0,0 +1,11 @@
+var x = 1 + 1 * 2;           // 3
+var y = (1 + 1) * 2;         // 4
+var z = 1 + (1 * 2);         // 3
+var a = 1 + 1 * 2 / 2;       // 2
+var b = (1 + 1 * 2) / 2;     // 1.5
+
+console.log("The value x: " + x);
+console.log("The value y: " + y);
+console.log("The value z: " + z);
+console.log("The value a: " + a);
+console.log("The value b: " + b);
